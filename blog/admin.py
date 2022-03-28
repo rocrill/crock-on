25d93442740c3e4aa8f498d708d07122666e9902
rocrill <crillyr@tcd.ie>
2022-03-28 21:03:53 +0000

    Start refining code with admin.py file.

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,13 @@
+"""Defines models for admin page."""
+
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """This class defines admin page Post model."""
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -14,11 +17,12 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """This class defines admin page Comment model."""
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email address', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
+        """This method approves comments."""
         queryset.update(approved=True)
-
